855bdcf235c061d0eba3c23833a75c1f34f5e333
Slenzy2 <tobiikupolati33@gmail.com>
2022-12-24 19:05:31 +0100

    Fixed Onboarding horizontal scroll issue

--- src/components/layouts/CustomerLayout.tsx
+++ src/components/layouts/CustomerLayout.tsx
@@ -2,19 +2,19 @@ import { CustomerLayoutProps } from "../../interfaces";
 import CustomerOnboarding from "../complex/CustomerOnboarding";
 import CustomerSidebar from "../compound/CustomerSidebar";
 function CustomerLayout({ children }: CustomerLayoutProps) {
-	return (
-		<div className="flex flex-row min-h-screen w-screen">
-			{/* Sidebar */}
-			<div className="flex-[0.3] h-inherit md:block hidden">
-				<CustomerSidebar />
-			</div>
-			{/* Body */}
-			<div className="flex-1">
-				<CustomerOnboarding />
-				{/* {children} */}
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex flex-row min-h-screen w-full">
+      {/* Sidebar */}
+      <div className="flex-[0.3] h-inherit md:block hidden">
+        <CustomerSidebar />
+      </div>
+      {/* Body */}
+      <div className="flex-1">
+        <CustomerOnboarding />
+        {/* {children} */}
+      </div>
+    </div>
+  );
 }
 
 export default CustomerLayout;
